6480322ef966cca6ce3c36fc38bf725997c98b19
@Geovanni_JDA <109145436+GeovanniJDA@users.noreply.github.com>
2025-07-04 17:01:13 -0300

    feat: implement Stripe account creation and add button in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { Stats } from "./_components/analytics";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import { getLoginOnboardAccount } from "./_data-access/create-onboard-account";
+import { CreateAccountButton } from "./_components/create-account-button";
 
 export default async function Dashboard() {
   const session = await auth();
@@ -19,11 +20,17 @@ export default async function Dashboard() {
         <div className="w-full flex items-center gap-2 justify-between">
           <h1 className="text-2xl font-semibold">Minha conta</h1>
 
-          <a className="bg-zinc-900 px-4 py-1 rounded-md text-white cursor-pointer">
-            Ajustar conta
-          </a>
+          { accountUrl && (
+            <a href={accountUrl} className="bg-zinc-900 px-4 py-1 rounded-md text-white cursor-pointer">
+              Ajustar conta
+            </a>
+          )}
         </div>
       </section>
+      
+      {!session.user.connectedStripeAccountId && (
+        <CreateAccountButton />
+      )}
 
       <Stats />
 
